92855eb41d872a332ba2216b3c3b52dddb815674
Judy Fan <judy.fan@sap.com>
2019-04-09 13:00:01 +0800

    backup

---
+++ japanese_replace.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    str = "含む";
+    print(str.replace("あ", "安c"));
+    print(str.replace("い", "以c"));
+    print(str.replace("う", "宇c"));
+    print(str.replace("え", "衣c"));
+    print(str.replace("お", "於c"));
+    print(str.replace("あいうえお", "安c"));
+    print(str.replace("む", "武c"));
+    # print(str.replace("is", "was", 3));
